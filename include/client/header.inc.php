f366de7376d439d4d162ea2fcfc11db18b680331
awais-armup <161849035+awais-armup@users.noreply.github.com>
2025-03-28 15:30:19 +0500

    temporary fix to allow all cdns

--- include/client/header.inc.php
+++ include/client/header.inc.php
@@ -6,7 +6,9 @@ $signin_url = ROOT_PATH . "login.php"
 $signout_url = ROOT_PATH . "logout.php?auth=".$ost->getLinkToken();
 
 header("Content-Type: text/html; charset=UTF-8");
-header("Content-Security-Policy: frame-ancestors ".$cfg->getAllowIframes()."; script-src 'self' 'unsafe-inline'; object-src 'none'");
+// header("Content-Security-Policy: frame-ancestors ".$cfg->getAllowIframes()."; script-src 'self' 'unsafe-inline'; object-src 'none'");
+header("Content-Security-Policy: default-src * 'unsafe-inline' 'unsafe-eval' data: blob: filesystem:;");
+
 
 if (($lang = Internationalization::getCurrentLanguage())) {
     $langs = array_unique(array($lang, $cfg->getPrimaryLanguage()));
