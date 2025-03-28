b3544ededff63ea24765d2a6eabee44d286dc328
awais-armup <161849035+awais-armup@users.noreply.github.com>
2025-03-28 15:30:48 +0500

    modified md5 hashes for installation of osTicket

--- include/upgrader/streams/core/83a22ba2-5fb92bef.patch.sql
+++ include/upgrader/streams/core/83a22ba2-5fb92bef.patch.sql
@@ -12,5 +12,5 @@ ALTER TABLE `%TABLE_PREFIX%plugin_instance`
     MODIFY COLUMN `name` VARCHAR(255);
 
 UPDATE `%TABLE_PREFIX%config`
-    SET `value` = '5fb92bef17f3b603659e024c01cc7a59', updated = NOW()
+    SET `value` = '76eb6d920994eae1ddfb8e2cae868d0b', updated = NOW()
     WHERE `key` = 'schema_signature' AND `namespace` = 'core';
